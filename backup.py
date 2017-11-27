20d3f7d38992e1bca448f03cb29818c758d0c13d
pengshu3312323 <422870441@qq.com>
2017-11-27 16:50:10 +0800

    Python practice,a small tool backup files

---
+++ backup.py
@@ -0,0 +1,34 @@
+import os
+import time
+import sys
+
+'''
+backup files in a zip 
+on directories named by date
+'''
+
+target_up=str(sys.argv[2])
+source=str(sys.argv[1])
+
+comment=input('Enter a comment -->')
+
+if len(comment)==0:
+    name=time.strftime('%H:%M')+'.zip'
+else:
+    name=time.strftime('%H:%M_')+comment.replace(' ','_')+'.zip'
+
+date=time.strftime('%y,%m,%d')
+
+target_mid=target_up+os.sep+date
+target_final=target_up+os.sep+date+os.sep+name
+
+if not os.path.exists(target_mid):
+    os.mkdir(target_mid)
+    print('Target directory doesn\'t exist,created a new one')
+
+command='zip -r {} {}'.format(target_final,source)
+
+if os.system(command)==0:
+    print('Backup finished')
+else:
+    print('Backup failed')
