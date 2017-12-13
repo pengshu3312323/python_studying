cc0981ca7529db42cec71da068f06a4eb9b21ea9
pengshu3312323 <422870441@qq.com>
2017-12-13 22:51:59 +0800

    I missed these...

--- mysite/meetpencil/settings.py
+++ mysite/meetpencil/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my app:
-    'blog'
+    'blog',
+    'guide',
 ]
 
 MIDDLEWARE = [
--- mysite/meetpencil/urls.py
+++ mysite/meetpencil/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls',namespace='blog')),
-]
+    path('guide/',include('guide.urls',namespace='guide')),
+    ]
