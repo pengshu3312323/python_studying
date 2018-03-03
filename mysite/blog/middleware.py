fcdbc5ba956f4a4e03f2092b87a6036349da82d2
pengshu3312323 <422870441@qq.com>
2018-03-03 22:48:04 +0800

    middleware_update

--- mysite/blog/middleware.py
+++ mysite/blog/middleware.py
@@ -7,9 +7,13 @@
 
 import sys
 
-from django.utils.deprecation import MiddlewareMixin
 from django.views.debug import technical_500_response
 from .models import VisitorNum
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddleMixin=object # Compatible with Django in old version
 
 class SuperuserDebugMiddleware(MiddlewareMixin):
     '''Only the superuser can see the debug info at exception'''
@@ -22,13 +26,15 @@ class VisitorNumCountMiddleware(MiddlewareMixin):
     '''Count the number of visitors'''
 
     def process_request(self,request):
-        num=VisitorNum.objects.filter(pk=1)
-        if len(num)==0:
-            new=VisitorNum()
-            new.numbers=1
-            new.save()
-        elif len(num)!=0:
-            new=num[0]
-            new.numbers+=1
-            new.save()
+        path=request.get_full_path()
+        if path=='/':
+            num=VisitorNum.objects.filter(pk=1)
+            if len(num)!=0:
+                new=num[0]
+                new.numbers+=1
+                new.save()
+            elif len(num)!=0:
+                new=VisitorNum()
+                new.numbers=1
+                new.save()
             
